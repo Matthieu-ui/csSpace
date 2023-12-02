2590ec102f3e63b43b2e9f38e542f6496577c031
“matthieu-ui” <mattfelker206@outlook.com>
2023-12-02 08:53:28 -0800

    content update-sm

--- lib/demos.ts
+++ lib/demos.ts
@@ -6,100 +6,58 @@ export type Item = {
 
 export const demos: { name: string; items: Item[] }[] = [
   {
-    name: 'Layouts',
+    name: 'Market',
     items: [
       {
-        name: 'Nested Layouts',
+        name: 'Inventory',
         slug: 'layouts',
-        description: 'Create UI that is shared across routes',
+        description: 'view active inventory w/ market data, pricing, etc.',
       },
       {
-        name: 'Grouped Layouts',
+        name: 'Trade',
         slug: 'route-groups',
         description: 'Organize routes without affecting URL paths',
       },
-      {
-        name: 'Parallel Routes',
-        slug: 'parallel-routes',
-        description: 'Render multiple pages in the same layout',
-      },
     ],
   },
   {
-    name: 'File Conventions',
+    name: 'CS2 Surf',
     items: [
       {
-        name: 'Loading',
+        name: 'Server Browser',
         slug: 'loading',
         description:
-          'Create meaningful Loading UI for specific parts of an app',
+          'Customized CS2 server list with filtering, sorting, and pagination',
       },
       {
-        name: 'Error',
+        name: 'Discord',
         slug: 'error-handling',
-        description: 'Create Error UI for specific parts of an app',
-      },
-      {
-        name: 'Not Found',
-        slug: 'not-found',
-        description: 'Create Not Found UI for specific parts of an app',
+        description: 'Join the Discord server',
       },
     ],
   },
   {
-    name: 'Data Fetching',
+    name: 'Counter-Strike News',
     items: [
       {
-        name: 'Streaming with Suspense',
+        name: 'Release Notes',
         slug: 'streaming',
-        description:
-          'Streaming data fetching from the server with React Suspense',
+        description: 'Official Counter-Strike: Global Offensive Release Notes',
       },
       {
-        name: 'Static Data',
+        name: 'Latest News',
         slug: 'ssg',
-        description: 'Generate static pages',
-      },
-      {
-        name: 'Dynamic Data',
-        slug: 'ssr',
-        description: 'Server-render pages',
-      },
-      {
-        name: 'Incremental Static Regeneration',
-        slug: 'isr',
-        description: 'Get the best of both worlds between static & dynamic',
+        description: 'conspiracy theories, patch notes, and more',
       },
     ],
   },
   {
-    name: 'Components',
+    name: 'Donate',
     items: [
       {
-        name: 'Client Context',
+        name: 'Support our work',
         slug: 'context',
-        description:
-          'Pass context between Client Components that cross Server/Client Component boundary',
-      },
-    ],
-  },
-  {
-    name: 'Misc',
-    items: [
-      {
-        name: 'Client Component Hooks',
-        slug: 'hooks',
-        description: 'Preview the routing hooks available in Client Components',
-      },
-      {
-        name: 'CSS and CSS-in-JS',
-        slug: 'styling',
-        description: 'Preview the supported styling solutions',
-      },
-      {
-        name: 'Code Snippets',
-        slug: 'snippets',
-        description: 'A collection of useful App Router code snippets',
+        description: 'Help continue to build and maintain the CS2 community',
       },
     ],
   },
